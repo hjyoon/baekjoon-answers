3748c957493aac2bcc82b7fc99fbb6617cf1eb29
Hyojeon Yoon <se1620236@naver.com>
2021-01-25 17:34:45 +0900

    update

---
+++ 11098.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open("input.txt", 'r')
+
+T = int(input())
+
+for _ in range(T):
+    P = int(input())
+    tmp = []
+    for _ in range(P):
+        C, NAME = input().split(); C = int(C)
+        tmp.append((C, NAME))
+    tmp.sort(reverse=True)
+    print(tmp[0][1])
+        
