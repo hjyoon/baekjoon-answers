98ab18dbfee082ae4c5550e0d653d3ed6c28e8b3
Hyojeon Yoon <se1620236@naver.com>
2021-01-25 20:42:40 +0900

    Update 2711.py

--- 2711.py
+++ 2711.py
@@ -2,15 +2,8 @@ import sys
 sys.stdin = open("input.txt", 'r')
 
 l = []
-def list_init():
-    cnt = 0
-    while True:
-        for i in range(1, 46):
-            for _ in range(i):
-                cnt+=1
-                l.append(i)
-                if cnt == 1000:
-                    return
-list_init()
+for i in range(1, 46):
+    l += [i]*i
+
 a, b = map(int, input().split())
 print(sum(l[a-1:b]))
