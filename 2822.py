6c180cec7ed720537efa12a456a8ff4ac382a22c
Hyojeon Yoon <se1620236@naver.com>
2021-01-27 00:15:25 +0900

    Update 2822.py

--- 2822.py
+++ 2822.py
@@ -1,7 +1,15 @@
 import sys
 sys.stdin = open("input.txt", 'r')
 
-for _ in range(8):
-    n = int(input())
-    l = list(map(int, input().split()))
-    print((max(l)-min(l))*2)
+l = list(map(lambda x:int(x.strip()), sys.stdin))
+l2 = []
+for i, v in enumerate(l):
+    l2.append((v, i+1))
+
+l2.sort(reverse=True)
+l3 = l2[:5]
+l3.sort(key=lambda x:x[1])
+l4 = list(map(lambda x:x[0], l3))
+print(sum(l4))
+l5 = list(map(lambda x:str(x[1]), l3))
+print(' '.join(l5))
