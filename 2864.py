fde8446b47f3ec16302108f4ea0845e008812ffe
Hyojeon Yoon <se1620236@naver.com>
2021-02-02 03:29:40 +0900

    update

---
+++ 2864.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdin = open("input.txt", 'r')
+
+A, B = input().split()
+A = A.replace('6', '5')
+B = B.replace('6', '5')
+print(int(A)+int(B), '', end='')
+A = A.replace('5', '6')
+B = B.replace('5', '6')
+print(int(A)+int(B))
