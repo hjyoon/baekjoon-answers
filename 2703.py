77686b4b919929a757892d6368887717d774e46c
Hyojeon Yoon <se1620236@naver.com>
2021-03-05 19:00:35 +0900

    Create 2703.py

---
+++ 2703.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open("input.txt", 'r')
+input = sys.stdin.readline
+
+T = int(input())
+for i in range(T):
+    s = input().rstrip()
+    l = input().rstrip()
+    #print(s.translate(''.maketrans('ABCDEFGHIJKLMNOPQRSTUVWXYZ', l)))
+    #print(s.translate(''.maketrans(''.join([chr(i) for i in range(65, 91)]), l)))
+    print(s.translate(''.maketrans(''.join(map(chr, range(65, 91))), l)))
