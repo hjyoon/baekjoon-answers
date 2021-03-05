2710966afa4090827e7b5eff86a67c52b7a906b6
Hyojeon Yoon <se1620236@naver.com>
2021-03-05 15:03:24 +0900

    Update 1269.py

--- 1269.py
+++ 1269.py
@@ -3,6 +3,6 @@ sys.stdin = open("input.txt", 'r')
 input = sys.stdin.readline
 
 _ = input()
-A = set(map(int, input().split()))
-B = set(map(int, input().split()))
-print(len(A.difference(B)) + len(B.difference(A)))
+A = set(input().split())
+B = set(input().split())
+print(len(A^B))
