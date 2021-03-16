ba1f76b2a379ce812fbbb552f11fc66ff132e89b
Hyojeon Yoon <se1620236@naver.com>
2021-03-17 08:34:24 +0900

    Update 1834.py

--- 1834.py
+++ 1834.py
@@ -3,7 +3,8 @@ sys.stdin = open("input.txt", 'r')
 input = sys.stdin.readline
 
 N = int(input())
-sum = 0
-for i in range(1, N):
-    sum += N*i+i
-print(sum)
+# sum = 0
+# for i in range(1, N):
+#     sum += N*i+i
+# print(sum)
+print((N**3-N)//2)
