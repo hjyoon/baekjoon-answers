c4089e0af4d7cdb66d272da1113e046c0ff463be
Hyojeon Yoon <se1620236@naver.com>
2021-03-23 02:25:24 +0900

    Update 2721.py

--- 2721.py
+++ 2721.py
@@ -5,10 +5,17 @@ input = sys.stdin.readline
 def T(n):
     return (n**2+n)//2
 
+d = [0]*301
+
 _, *S = map(lambda x:x.rstrip(), sys.stdin)
 
 for v in S:
+    v = int(v)
+    if d[v] != 0:
+        print(d[v])
+        continue
     sum = 0
-    for i in range(1, int(v)+1):
+    for i in range(1, v+1):
         sum += i*T(i+1)
-    print(sum)
+    d[v] = sum
+    print(d[v])
