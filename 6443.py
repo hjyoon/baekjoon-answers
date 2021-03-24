2d9901cbe440c4f3c7febd28f6501a6b078a6dc9
Hyojeon Yoon <se1620236@naver.com>
2021-03-25 07:04:30 +0900

    Create 6443.py

---
+++ 6443.py
@@ -0,0 +1,27 @@
+import sys
+sys.stdin = open("input.txt", 'r')
+input = sys.stdin.readline
+
+def nextPerm(s):
+    i = len(s)-2
+    j = 0
+    while i >= 0 and s[i] >= s[i+1]:
+        i -= 1
+    
+    if i == -1:
+        return None
+    else:
+        j = i+1
+        while j < len(s) and s[j] > s[i]:
+            j += 1
+        s[i], s[j-1] = s[j-1], s[i]
+        return s[:i+1] + s[:i:-1]
+
+_, *S = map(lambda x:x.rstrip(), sys.stdin)
+for v in S:
+    v = sorted(v)
+    while True:
+        if v == None:
+            break
+        print(''.join(v))
+        v = nextPerm(v)
