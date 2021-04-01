b73ec7497eb4bb996ad21c46b8f3366413058987
Hyojeon Yoon <se1620236@naver.com>
2021-04-02 04:07:42 +0900

    Create 2485.py

---
+++ 2485.py
@@ -0,0 +1,24 @@
+import sys
+sys.stdin = open("input.txt", 'r')
+input = sys.stdin.readline
+
+import math
+
+_, *S = map(lambda x:int(x.rstrip()), sys.stdin)
+print(S)
+assert S[-1] != '\n'
+
+
+
+S = sorted(S[:_])
+t = []
+a = S[0]
+for i in range(1, len(S)):
+    b = S[i]
+    t.append(b-a)
+    a = b
+
+r = math.gcd(*t)
+#print(sum(t)//r-len(t))
+print(sum(i//r-1 for i in t))
+
