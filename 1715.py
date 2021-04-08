cdc415d7f4de1fdfec17f17cc76446fbb68972df
Hyojeon Yoon <se1620236@naver.com>
2021-04-09 01:41:56 +0900

    Create 1715.py

---
+++ 1715.py
@@ -0,0 +1,24 @@
+import sys
+sys.stdin = open("input.txt", 'r')
+input = sys.stdin.readline
+
+import heapq
+
+N = int(input().rstrip())
+S = []
+for i in range(N):
+    S.append(int(input().rstrip()))
+
+heapq.heapify(S)
+if len(S) == 1:
+    print(0)
+    exit()
+
+r = 0
+while len(S) > 1:
+    a = heapq.heappop(S)
+    b = heapq.heappop(S)
+    heapq.heappush(S, a+b)
+    r += a+b
+    print(S)
+print(r)
