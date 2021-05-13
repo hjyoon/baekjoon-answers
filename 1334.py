303ba7cb6b8003ac22fa99c15570abd9d7b92e92
Hyojeon Yoon <se1620236@naver.com>
2021-05-13 14:04:49 +0900

    Update 1334.py

--- 1334.py
+++ 1334.py
@@ -6,32 +6,31 @@ N = input().rstrip()
 
 if N == '9':
     print(11)
-else:
-    if len(N) % 2 == 0:
-        left = N[:len(N)//2]
-        right = N[len(N)//2:]
-        c1 = int(left + left[::-1])
-        c2 = None
-        t = str(int(left)+1)
-        if len(t) == len(left):
-            c2 = int(t + t[::-1])
-        else:
-            c2 = int(t + t[::-1][1:])
-        if int(N) < c1:
-            print(c1)
-        else:
-            print(c2)
+    exit(0)
+
+c1 = None
+c2 = None
+
+if len(N) % 2 == 0:
+    left = N[:len(N)//2]
+    right = N[len(N)//2:]
+    c1 = int(left + left[::-1])
+    t = str(int(left)+1)
+    if len(t) == len(left):
+        c2 = int(t + t[::-1])
     else:
-        left = N[:len(N)//2+1]
-        right = N[len(N)//2+1:]
-        c1 = int(left + left[::-1][1:])
-        c2 = None
-        t = str(int(left)+1)
-        if len(t) == len(left):
-            c2 = int(t + t[::-1][1:])
-        else:
-            c2 = int(t + t[::-1][2:])
-        if int(N) < c1:
-            print(c1)
-        else:
-            print(c2)
+        c2 = int(t + t[::-1][1:])
+else:
+    left = N[:len(N)//2+1]
+    right = N[len(N)//2+1:]
+    c1 = int(left + left[::-1][1:])
+    t = str(int(left)+1)
+    if len(t) == len(left):
+        c2 = int(t + t[::-1][1:])
+    else:
+        c2 = int(t + t[::-1][2:])
+
+if int(N) < c1:
+    print(c1)
+else:
+    print(c2)
