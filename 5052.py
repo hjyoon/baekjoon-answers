39a652693fa4795ab75e43308a586933dd27ffdf
Hyojeon Yoon <se1620236@naver.com>
2021-06-25 00:11:42 +0900

    Update 5052.py

--- 5052.py
+++ 5052.py
@@ -29,22 +29,6 @@ def read_data(l, in_f, out_f=None):
     else:
         l.append({'data':data})
 
-def computeLPS(pat, lps):
-    leng = 0
-
-    i = 1
-    while i < len(pat):
-        if pat[i] == pat[leng]:
-            leng += 1
-            lps[i] = leng
-            i += 1
-        else:
-            if leng != 0:
-                leng = lps[leng-1]
-            else:
-                lps[i] = 0
-                i += 1
-
 def solution(T, L):
     ans = []
     for _, l in L:
