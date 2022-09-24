4160cfbb454e6160d980d91cfeb2f57e6df3a123
hjyoon <se1620236@naver.com>
2022-09-25 01:47:14 +0900

    upate

---
+++ _8000/8871.js
@@ -0,0 +1,14 @@
+"strict mode";
+const stdin = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trimEnd()
+  .split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const N = Number(input());
+
+console.log((N + 1) * 2, (N + 1) * 3);
