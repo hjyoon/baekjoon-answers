76c00e3e5130bc0fb2765a65af33a704fc9c7a91
hjyoon <se1620236@naver.com>
2022-10-23 23:07:28 +0900

    update

---
+++ _23000/23234.js
@@ -0,0 +1,12 @@
+"strict mode";
+const stdin = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trimEnd()
+  .split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+console.log("The world says hello!");
